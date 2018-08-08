34bf7f9535483a51cf9a6bbb3dd2f023ce6d7889
pro <zczhaozc>
2018-08-08 22:24:44 +0800

    redux版本

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./saving";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/saving.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+function saving(state = 0, action) {
+  switch (action.type) {
+    case "save-money":
+      return state + 1 * 100;
+    case "withdraw-money":
+      return state - 1 * 100;
+    default:
+      return 0;
+  }
+}
+
+const store = createStore(
+  saving,
+  //使用chrome redux插件
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const init = store.getState();
+console.log(`一开始有${init}元`);
+
+function listener() {
+  const current = store.getState();
+  console.log(`现在有${current}元`);
+}
+
+store.subscribe(listener);
+
+store.dispatch({ type: "save-money" });
+store.dispatch({ type: "save-money" });
+store.dispatch({ type: "save-money" });
+store.dispatch({ type: "withdraw-money" });
